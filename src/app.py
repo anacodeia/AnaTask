8c27d4596b40463534dd56ce5b2b1c4898ac9d03
Ana Cecília Batista <anaceciliabatista62@gmail.com>
2026-04-09 10:20:40 -0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,15 +1,29 @@
 tarefas = []
+
 while True:
-    print("1 - Adicionar tarefa")
+    print("\n1 - Adicionar tarefa")
     print("2 - Listar tarefas")
     print("3 - Sair")
 
-    opcao = input("Digite a tarefa:")
+    opcao = input("Digite a opção: ")
+
     if opcao == "1":
-        tarefa = input("Digite a tarefa:")
+        tarefa = input("Digite a tarefa: ")
 
         if tarefa == "":
             print("Tarefa inválida!")
         else:
             tarefas.append(tarefa)
-            print("Tarefa adicional!")
+            print("Tarefa adicionada!")
+
+    elif opcao == "2":
+        print("\nLista de tarefas:")
+        for t in tarefas:
+            print("-", t)
+
+    elif opcao == "3":
+        print("Saindo...")
+        break
+
+    else:
+        print("Opção inválida!")
