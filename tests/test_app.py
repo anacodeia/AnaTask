3fb7ac6fffc0b55a4d16467311707216c3fcb10f
Ana Cecília Batista <anaceciliabatista62@gmail.com>
2026-04-09 10:24:02 -0300

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,2 +1,17 @@
-def test_simples():
-    assert 1 + 1 == 2
+from src.app import adicionar_tarefa
+
+
+def test_adicionar_tarefa_valida():
+    lista = []
+    resultado = adicionar_tarefa(lista, "Estudar")
+
+    assert resultado == True
+    assert lista == ["Estudar"]
+
+
+def test_adicionar_tarefa_vazia():
+    lista = []
+    resultado = adicionar_tarefa(lista, "")
+
+    assert resultado == False
+    assert lista == []
